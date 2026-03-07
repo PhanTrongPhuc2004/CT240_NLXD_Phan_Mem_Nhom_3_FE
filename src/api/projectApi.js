b80ffc3590668a7109471ab10fb612ae05747abd
Lomos1901 <socerbook@gmail.com>
2026-03-07 16:55:02 +0700

    them show cua admin

--- src/api/projectApi.js
+++ src/api/projectApi.js
@@ -2,11 +2,15 @@
 import api from './index'
 
 export const projectApi = {
-  getAll: () => api.get('/projects'),
+  // --- API CHO NGƯỜI DÙNG CÁ NHÂN ---
+  getAll: () => api.get('/projects'), // Lấy các dự án của người dùng hiện tại
   getById: (id) => api.get(`/projects/${id}`),
   create: (data) => api.post('/projects', data),
   update: (id, data) => api.put(`/projects/${id}`, data),
   delete: (id) => api.delete(`/projects/${id}`),
+
+  // --- API CHO ADMIN/MANAGER ---
+  getAllSystem: () => api.get('/projects/all-system'), // <-- DÒNG MỚI ĐƯỢC THÊM
 
   // --- Bổ sung các hàm cho CN_15 & CN_16 ---
 
